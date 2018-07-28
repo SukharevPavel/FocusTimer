fed3a234d98f8e3107e2b2ed6fd603ce6ddc4d6a
Pavel Sukharev <suharev_p@mail.ru>
2018-07-28 16:16:23 +0300

    Initial commit

---
+++ app/src/main/java/ru/sukharev/focustimer/FocusActivity.kt
@@ -0,0 +1,12 @@
+package ru.sukharev.focustimer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FocusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_focus)
+    }
+}
